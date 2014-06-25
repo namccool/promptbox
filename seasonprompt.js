d2ff0e97998158ac91f3f854816c566300711d97
Nellie Ann McCool <namccool@gmail.com>
2014-06-25 10:32:35 -0700

    show question. allow caps/different names in input.

--- seasonprompt.js
+++ seasonprompt.js
@@ -2,28 +2,30 @@ var body = $("body");
 var input = $("input");
 
 function append(el) {
+	body.append(el.choice);
 	body.append(el.months);
 	body.append(el.holidays);
 }
 
-function Season(months, holidays, color) {
+function Season(choice, months, holidays, color) {
+	this.choice = "<h1>" + choice + "</h1>";
 	this.months = "<h1>" + months.join(", ") + "</h1>";
 	this.holidays = "<h1>" + holidays.join(", ") + "</h1>";
 	this.color = body.css("background", color);
 }
 
 function printSeason(interest) {
-	var season = new Season(interest[0], interest[1], interest[2]);
+	var season = new Season(interest[0], interest[1], interest[2], interest[3]);
 	append(season);
 }
 
-var winter = [["December", "January", "February"], ["Hannukah", "Ramadan", "Christmas", "Kwanza", "New Year"], "lightBlue"];
+var winter = [["WINTER"],["December", "January", "February"], ["Hannukah", "Ramadan", "Christmas", "Kwanza", "New Year"], "lightBlue"];
 
-var spring = [["March", "April", "May"], ["Mardi Gras", "Easter", "Purim", "Passover", "Earth Day", "Cinco De Mayo", "Mother's Day"], "green"]
+var spring = [["SPRING"],["March", "April", "May"], ["Mardi Gras", "Easter", "Purim", "Passover", "Earth Day", "Cinco De Mayo", "Mother's Day"], "green"]
 
-var summer = [["June", "July", "August"], ["Father's Day", "Independence Day"], "yellow"]
+var summer = [["SUMMER"],["June", "July", "August"], ["Father's Day", "Independence Day"], "yellow"]
 
-var autumn = [["September", "October", "November"], ["Rosh Hashanah", "Yom Kippur", "Columbus Day", "Halloween", "Day of the Dead", "All Soul's Day", "Veteran's Day", "Thanksgiving"], "red"]
+var autumn = [["AUTUMN"],["September", "October", "November"], ["Rosh Hashanah", "Yom Kippur", "Columbus Day", "Halloween", "Day of the Dead", "All Soul's Day", "Veteran's Day", "Thanksgiving"], "red"]
 
 var seasons = {
 	winter: winter,
@@ -34,9 +36,16 @@ var seasons = {
 
 input.on("change", function() {
 	console.log(this);
+	this.value = this.value.toLowerCase();
 	if (!seasons[this.value]) {
-		body.append(this.value + " is not a season! Try again.")
-		return
+		if (this.value === "fall") {
+			body.append("Did you mean Autumn? Try again.")
+			return
+		}
+		else {
+			body.append(this.value + " is not a season! Try again.")
+			return
+		}
 	}
 	printSeason(seasons[this.value]);
 });
